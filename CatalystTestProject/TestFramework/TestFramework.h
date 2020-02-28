3d5ab2aaedfa55255d8709ea763c88861ce57d94
Dawon <wekdnjs@gmail.com>
2020-02-28 19:59:50 +0900

    framework add

---
+++ CatalystTestProject/TestFramework/TestFramework.h
@@ -0,0 +1,19 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by Dawon on 2020/02/28.
+//  Copyright © 2020 Dawon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
