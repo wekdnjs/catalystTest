4284db99d9dce25b7c92e0fc5316228253596325
Dawon <wekdnjs@gmail.com>
2020-02-29 15:15:34 +0900

    script commit

---
+++ CatalystTestProject/scripts/modify.sh
@@ -0,0 +1,16 @@
+filepath="../CatalystTestProject/CatalystTestProject.xcodeproj"
+searchstring="buildSettings = {"
+replacestring="buildSettings = { SUPPORTS_MACCATALYST = YES;"
+
+i=0;
+
+for file in $(grep -l -R $searchstring $filepath)
+do
+  cp $file $file.bak
+  sed -e "s/$searchstring/$replacestring/ig" $file > project.pbxproj
+  mv project.pbxproj $file
+
+  let i++;
+
+  echo "Modified: " $file
+done
