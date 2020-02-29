a71385db5b2f0aa650dba9400887cbec06d1fdfc
Dawon <wekdnjs@gmail.com>
2020-02-29 16:10:32 +0900

    edit02

--- CatalystTestProject/scripts/modify.sh
+++ CatalystTestProject/scripts/modify.sh
@@ -1,20 +1,22 @@
 #filepath="../CatalystTestProject.xcodeproj" /CatalystTestProject/
-filepath="./CatalystTestProject/CatalystTestProject.xcodeproj"
+filepath="./CatalystTestProject/CatalystTestProject.xcodeproj/project.pbxproj"
 searchstring="buildSettings = {"
 replacestring="buildSettings = { SUPPORTS_MACCATALYST = YES;"
 
-i=0;
-all=$(grep -l -R $searchstring $filepath)
-
-echo "all: " $all
-
-for file in $all
-do
-  cp $file $file.bak
-  sed -e "s/$searchstring/$replacestring/ig" $file > project.pbxproj
-  mv project.pbxproj $file
-
-  let i++;
-
-  echo "Modified: " $file
-done
+sed "s/$searchstring/$replacestring/g" $filepath
+echo "Modified: " $(cat $filepath)
+#i=0;
+#all=$(grep -l -R $searchstring $filepath)
+#
+#echo "all: " $all
+#
+#for file in $all
+#do
+#  cp $file $file.bak
+#  sed -e "s/$searchstring/$replacestring/ig" $file > project.pbxproj
+#  mv project.pbxproj $file
+#
+#  let i++;
+#
+#  echo "Modified: " $file
+#done
