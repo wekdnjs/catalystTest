766d4225378620e2b026408ee937ff34814f078b
Dawon <wekdnjs@gmail.com>
2020-03-01 14:53:47 +0900

    ㅜ

--- CatalystTestProject/scripts/modify.sh
+++ CatalystTestProject/scripts/modify.sh
@@ -1,10 +1,12 @@
 #filepath="../CatalystTestProject.xcodeproj" /CatalystTestProject/
 filepath="./CatalystTestProject/CatalystTestProject.xcodeproj/project.pbxproj"
 searchstring="buildSettings = {"
-replacestring="buildSettings = { SUPPORTS_MACCATALYST = YES;                 DERIVE_MACCATALYST_PRODUCT_BUNDLE_IDENTIFIER = YES;"
+replacestring="buildSettings = { SUPPORTS_MACCATALYST = YES; DERIVE_MACCATALYST_PRODUCT_BUNDLE_IDENTIFIER = YES;"
 
-sed -i "s/$searchstring/$replacestring/g" $filepath
+sed -i "s\/$searchstring\/$replacestring\/g" $filepath
 echo "Modified: " $(cat $filepath)
+
+
 #i=0;
 #all=$(grep -l -R $searchstring $filepath)
 #
